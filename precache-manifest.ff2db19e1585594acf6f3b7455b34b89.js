8f61061c9724fc21e7eb6a35c1819d8f34940741
Arun Singh <arunsingh9251@gmail.com>
2021-04-01 15:17:01 +0530

    Updates

---
+++ precache-manifest.ff2db19e1585594acf6f3b7455b34b89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65266a12fd0aaf9fd6fefc9708c42768",
+    "url": "/Voice-to-TextNote-Converter-ReactApp/index.html"
+  },
+  {
+    "revision": "708a371f139cb893670e",
+    "url": "/Voice-to-TextNote-Converter-ReactApp/static/css/main.c52b6ba1.chunk.css"
+  },
+  {
+    "revision": "3b2d1b33a0e5f57bc25b",
+    "url": "/Voice-to-TextNote-Converter-ReactApp/static/js/2.a7fca69d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Voice-to-TextNote-Converter-ReactApp/static/js/2.a7fca69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708a371f139cb893670e",
+    "url": "/Voice-to-TextNote-Converter-ReactApp/static/js/main.cae62b78.chunk.js"
+  },
+  {
+    "revision": "c01d74728cd28c5b81e3",
+    "url": "/Voice-to-TextNote-Converter-ReactApp/static/js/runtime-main.3a44a44c.js"
+  }
+]);
